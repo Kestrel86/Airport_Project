1ee4ab7b02cc0456522ba0965d67fe0361714545
AndrewJohnValdez <andrewjohnv@cpp.edu>
2022-12-06 15:42:33 -0800

    Added csv files and added commnets

--- AirportApp.java
+++ AirportApp.java
@@ -1,9 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//linkedlist or adjacency matrix
+
 public class AirportApp {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws FileNotFoundException
     {
-        
+        File airports = new File("airports.csv");
+        File distance = new File("distances.csv");
+        Scanner scnr = new Scanner(airports);
+        scnr.useDelimiter(",");
+        while(scnr.hasNext())
+        {
+            System.out.println(scnr.next());
+        }
+        scnr.close();
+        //keep number of airports
+
+        scnr = new Scanner(distance);
+        while(scnr.hasNext())
+        {
+            System.out.println(scnr.next());
+        }
+        scnr.close();
     }
 }
+
+//make an airportInfo class
+//holds constructors
+//Will create a simple data structure that has the info for the airport and etc.
 
 //pg 1082
 
