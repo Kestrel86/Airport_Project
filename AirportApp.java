721f39ec42f0450ed4fd3c1a67f115d42d1ef435
Kestrel86 <valdezandrew20@gmail.com>
2022-12-08 19:49:00 -0800

    code reorder

--- AirportApp.java
+++ AirportApp.java
@@ -15,12 +15,12 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
 
-//Ask professor about Directed Graph name as well as other command conditions and output
-
 public class AirportApp 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws FileNotFoundException 
     { 
+        //DECLARING VALUES
+
         File airFile = new File("./airports.csv");
         File distFile = new File("./distances.csv");
         Scanner scnr = new Scanner(System.in);
@@ -30,35 +30,35 @@ public class AirportApp
 
         System.out.println("Airports v0.1 by A. Valdez");
 
-        try {
-            Scanner airportScnr = new Scanner(airFile);
-            Scanner distanceScnr = new Scanner(distFile);
+        //SCANNING AND ALLOCATING VALUES
 
-            while(airportScnr.hasNextLine())
-            {
-                AirportInfo airport = new AirportInfo(airportScnr.nextLine());
-                dictionary.add(airport.getCode(), airport); //create dictionary to store the values based on airport code
-                diGraph.addVertex(airport.getCode()); //grabs code from aiport data structure
-            }
-            
-            while(distanceScnr.hasNextLine())
-            {
-                String[] str = distanceScnr.nextLine().split(",");
-                diGraph.addEdge(str[0], str[1], Double.parseDouble(str[2]));
-            }
+        Scanner airportScnr = new Scanner(airFile);
+        Scanner distanceScnr = new Scanner(distFile);
 
-            airportScnr.close();
-            distanceScnr.close();
-
-        } catch(FileNotFoundException e) {
-            System.out.println("File not Found");
+        while(airportScnr.hasNextLine())
+        {
+            AirportInfo airport = new AirportInfo(airportScnr.nextLine());
+            dictionary.add(airport.getCode(), airport); //create dictionary to store the values based on airport code
+            diGraph.addVertex(airport.getCode()); //grabs code from aiport data structure
         }
         
-        do {
-            System.out.println("");
-            System.out.print("Command? ");
-            userInput = scnr.nextLine().toUpperCase().split(" ");
+        while(distanceScnr.hasNextLine())
+        {
+            String[] str = distanceScnr.nextLine().split(",");
+            diGraph.addEdge(str[0], str[1], Double.parseDouble(str[2]));
+        }
 
+        airportScnr.close();
+        distanceScnr.close();
+
+        //MAIN MENU CODE
+
+        System.out.println("");
+        System.out.print("Command? ");
+        userInput = scnr.nextLine().toUpperCase().split(" ");
+
+        while(!userInput[0].equals("E")) 
+        {
             switch(userInput[0])
             {
                 case "H":
@@ -73,9 +73,8 @@ public class AirportApp
                             System.out.println("Unknown Airport Code");
                             continue;
                         } else {
-                            System.out.print(userInput[i] + " - ");
                             AirportInfo airInfo = dictionary.getValue(userInput[i]);
-                            System.out.println(airInfo.toString());
+                            airInfo.printOut();
                         }
                     }
                     break;
@@ -85,16 +84,16 @@ public class AirportApp
                     {
                         Stack<String> stack = new Stack<>();
                         int cheapestPath = (int)diGraph.getCheapestPath(userInput[1], userInput[2], stack);
-                        if(cheapestPath == 0)
+                        if(cheapestPath != 0)
                         {
-                            System.out.println("Airports Are not Connected");
-                        } else {
                             System.out.println(dictionary.getValue(userInput[1]).toString() + " to " + dictionary.getValue(userInput[2]).toString() + " is " + cheapestPath + " through the route:");
                             while(!stack.isEmpty())
                             {
-                                AirportInfo startPt = dictionary.getValue(stack.pop());
-                                System.out.println(startPt.getCode() + " - " + startPt.toString());
+                                AirportInfo airInfo = dictionary.getValue(stack.pop());
+                                airInfo.printOut();
                             }
+                        } else {
+                            System.out.println("Airports Not Connected");
                         }
                     } else {
                         System.out.println("Unknown Airport Code");
@@ -102,48 +101,42 @@ public class AirportApp
                     break;
 
                 case "I": 
-                    int distance;
-                    try {
-                        distance = Integer.parseInt(userInput[3]);
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        distance = -1;
-                    } catch (NumberFormatException e) {
-                        distance = -1;
-                    }
+                    int distance = 0;
+                    distance = Integer.parseInt(userInput[3]);
 
                     if (distance > 0) {
-						if (dictionary.getValue(userInput[1]) != null && dictionary.getValue(userInput[2]) != null) {
+                        if (dictionary.getValue(userInput[1]) != null && dictionary.getValue(userInput[2]) != null) {
                             //Check if user inputted values then execute
-							boolean result = diGraph.addEdge(userInput[1], userInput[2], distance); //Return true if the new edge is created, false if already existing
+                            boolean result = diGraph.addEdge(userInput[1], userInput[2], distance); //Return true if the new edge is created, false if already existing
 
-							if (result == true) {
-								System.out.println(
-										dictionary.getValue(userInput[1]).toString() + " to "
-												+ dictionary.getValue(userInput[2]).toString()
-												+ " with a distance of " + distance + " added.");
-							} else {
-								System.out.println("The connection already exist.");
+                            if (result == true) {
+                                System.out.println(
+                                        dictionary.getValue(userInput[1]).toString() + " to "
+                                                + dictionary.getValue(userInput[2]).toString()
+                                                + " with a distance of " + distance + " added.");
+                            } else {
+                                System.out.println("Connection already exists.");
                             }
-						} else {
-							System.out.println("Unknown Airport Code");
+                        } else {
+                            System.out.println("Unknown Airport Code");
                         }
-					} else {
-						System.out.println("Failed: Distance is less than 0.");
+                    } else {
+                        System.out.println("Failed: Distance is less than or equal to 0.");
                     }
-					break;
+                    break;
                     
                 case "R": 
                     if(dictionary.getValue(userInput[1]) != null && dictionary.getValue(userInput[2]) != null) 
                     { //check dictionary for value from userInput
                         if(diGraph.hasEdge(userInput[1], userInput[2])) 
                         {//Check if edge exists to be deleted
-                            boolean result = diGraph.addEdge(userInput[1], userInput[2]); //probable cause
+                            boolean result = diGraph.addEdge(userInput[1], userInput[2]);
                             if(result == true) 
                             {
                                 System.out.println(dictionary.getValue(userInput[1]).toString() + " and " + dictionary.getValue(userInput[2]).toString() + " removed.");
                             }
                         } else {
-                            System.out.println("Airports Are Not Connected");
+                            System.out.println("Airports Not Connected");
                         }
                     } else {
                         System.out.println("Unknown Airport Code");
@@ -159,13 +152,15 @@ public class AirportApp
                     System.out.println("For Command List, Type: H");
                     break;
             }
-        } while(!userInput[0].equals("E"));
+            System.out.println("");
+            System.out.print("Command? ");
+            userInput = scnr.nextLine().toUpperCase().split(" ");
+        }
         scnr.close();
     }   
 
     private static void displayMenu()
     {
-        //System.out.println("H Print Menu");
         System.out.println("Q Query the airport information by entering the airport code.");
         System.out.println("D Find the minimum distance between two aiports.");
         System.out.println("I Insert a connection between two airports.");
--- AirportInfo.java
+++ AirportInfo.java
@@ -28,23 +28,6 @@ public class AirportInfo
         state = str[3];
     }
     
-    /* 
-    /**
-     * Constructor designed to store information based on the parameters.
-     * @param newCode Determines the new Code assigned "code"
-     * @param newCity Determines the new City assigned to "city"
-     * @param newName Determines the new Name assigned to "name"
-     * @param newState Determines the new State assigned to "state"
-     
-    public AirportInfo(String newCode, String newCity, String newName, String newState)
-    {
-        code = newCode;
-        city = newCity;
-        name = newName;
-        state = newState;
-    }
-    */
-
     /**
      * 
      * @return
@@ -72,5 +55,10 @@ public class AirportInfo
     public String toString()
     {
         return getCity() + " " + getName() + " " + getState();
+    }
+
+    public void printOut()
+    {
+        System.out.println(getCode() + " - " + getCity() + " " + getName() + " " + getState());
     }
 }
